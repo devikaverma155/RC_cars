64f50e819fb6fb5d19269b4fd84fb350b6bf247b
devikaverma155 <devikaverma1554@gmail.com>
2024-06-20 17:18:07 +0530

    footer

--- frontend/myapp/src/components/Footer.js
+++ frontend/myapp/src/components/Footer.js
@@ -16,16 +16,11 @@ function Footer() {
             <Container sx={{color:'black'}}>
              
          
-          <Typography  fontWeight={600} ><b> HackerComp</b></Typography> 
+          <Typography  fontWeight={600} ><b> Contact Us</b></Typography> 
           <br></br> 
-          <Link underline='none'> <Typography sx={{color:'black'}} > About</Typography>
+          <Link underline='none'> <Typography sx={{color:'black'}} >phone number</Typography>
           </Link>
-          <Link underline='none'> <Typography sx={{color:'black'}} >Careers</Typography>
-          </Link>
-          <Link underline='none'> <Typography sx={{color:'black'}} >Contact</Typography>
-          </Link>
-          <Link underline='none'> <Typography sx={{color:'black'}} >Help</Typography>
-          </Link>
+         
         
           
             </Container>
@@ -33,27 +28,18 @@ function Footer() {
           <Grid item xs={12} sm={6} md={4} lg={2} >
             <Container>
             <Container sx={{display:'flex',flexDirection:'column',alignItems:'center'}}></Container>
-            <Typography  fontWeight={600} ><b> Hackathons</b>  </Typography> 
+            <Typography  fontWeight={600} ><b>Cars</b>  </Typography> 
             <br></br>
-          <Link href="/Hackathon" underline='none'sx={{color:'black'}} > Browse hackathons
-          </Link><br></br>
-          <Link underline='none'> <Typography sx={{color:'black'}} >Create Teams</Typography>
+          <Link href="/carslisting" underline='none'sx={{color:'black'}} > View Cars
           </Link>
-        <Link underline='none'> <Typography sx={{color:'black'}} >Resources</Typography>
-          </Link>
-        <Link underline='none'> <Typography sx={{color:'black'}} >Search for Teams</Typography>
-          </Link>
+          
             </Container>
           </Grid>
           <Grid item xs={12} sm={6} md={4} lg={2} >
             <Container>
-            <Typography  fontWeight={600} > <b> Protfolio</b> </Typography>
+            <Typography  fontWeight={600} > <b> Address</b> </Typography>
             <br></br>  
-          <Link underline='none'> <Typography sx={{color:'black'}} >Your hackathon</Typography>
-          </Link>
-          <Link underline='none'> <Typography sx={{color:'black'}} >Your Profile</Typography>
-          </Link>
-        <Link underline='none'> <Typography sx={{color:'black'}} >Your Team</Typography>
+          <Link underline='none'> <Typography sx={{color:'black'}} >Shop address</Typography>
           </Link>
           
             </Container>
